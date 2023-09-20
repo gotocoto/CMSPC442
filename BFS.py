4244f67728cf06cb51a59c7179847d9a21344813
Ben House <bjh6066@psu.edu>
2023-09-20 13:11:12 -0400

    Add the code

--- BFS.py
+++ BFS.py
@@ -4,6 +4,7 @@ def read_file(file):
 import numpy as np
 import pygame
 import heapq
+import queue
 size = 150
 border = 5
 background_color=[180,180,180]
@@ -32,10 +33,11 @@ class Tile(object):
             surf.blit(self.tile, self.center)
 #p  from queue import PriorityQueue
 #targetGrid = np.array([[0,1,2],[3,4,5],[6,7,8]])
-targetGrid = [0,1,2,3,4,5,6,7,8]
+targetGrid = '012345678'
 #startingGrid = np.array([[1,0,2],[3,4,5],[6,7,8]])
 #startingGrid = [np.array([[7,2,4],[5,0,6],[8,3,1]])]
-startingGrid= [2,1,0,3,4,5,6,7,8]
+#startingGrid= '724506831'
+startingGrid = '123456078'
 #startingGrid = [7,2,4,5,0,6,8,3,1]
 def getMd(grid,targetGrid):
     md = 0
@@ -48,43 +50,35 @@ def getMd(grid,targetGrid):
     return(md)
 def getEmpty(grid):
     for i in range(9):
-        if(grid[i]==0):
+        if(grid[i]=='0'):
             return i
-def moveUp(grid,path):
+def moveUp(grid,path,empty):
     path+='U'
-    empty = getEmpty(grid)
-    grid[empty]=grid[empty-3]
-    grid[empty-3]=0
-    return [grid,path]
-def moveDown(grid,path):
+    newGrid = grid[:empty-3]+'0'+grid[empty-2:empty]+grid[empty-3]+grid[empty+1:]
+    return [newGrid,path]
+def moveDown(grid,path,empty):
     path+='D'
-    empty = getEmpty(grid)
-    grid[empty]=grid[empty+3]
-    grid[empty+3]=0
-    return [grid,path]
-def moveLeft(grid,path):
+    newGrid = grid[:empty]+grid[empty+3]+grid[empty+1:empty+3]+'0'+grid[empty+4:]
+    return [newGrid,path]
+def moveLeft(grid,path,empty):
     path+='L'
-    empty = getEmpty(grid)
-    grid[empty]=grid[empty-1]
-    grid[empty-1]=0
-    return [grid,path]
-def moveRight(grid,path):
+    newGrid = grid[:empty-1]+'0'+grid[empty-1]+grid[empty+1:]
+    return [newGrid,path]
+def moveRight(grid,path,empty):
     path+='R'
-    empty = getEmpty(grid)
-    grid[empty]=grid[empty+1]
-    grid[empty+1]=0
-    return [grid,path]
+    newGrid = grid[:empty]+grid[empty+1]+'0'+grid[empty+2:]
+    return [newGrid,path]
 def branch(grid,path):
     branches = []
     empty = getEmpty(grid)
     if(empty%3 != 2):
-        branches.append(moveRight(list.copy(grid),path))
+        branches.append(moveRight(grid,path,empty))
     if(empty%3 != 0 ):
-        branches.append(moveLeft(list.copy(grid),path))
+        branches.append(moveLeft(grid,path,empty))
     if(empty>2):
-        branches.append(moveUp(list.copy(grid),path))
+        branches.append(moveUp(grid,path,empty))
     if(empty<6):
-        branches.append(moveDown(list.copy(grid),path))
+        branches.append(moveDown(grid,path,empty))
     return branches #Remove the None types
 class gridPath:
     def __init__(self,combination):
@@ -101,27 +95,51 @@ class gridPath:
         return [self.grid,self.path]
 #print(getMd(startingGrid,targetGrid))
 print(startingGrid)
-print(moveUp(startingGrid,[]))
-print(branch(startingGrid,[]))
-print(getEmpty(startingGrid))
+print(moveUp(startingGrid,'',getEmpty(startingGrid)))
+print(branch(startingGrid,''),getEmpty(startingGrid))
+print(getEmpty(startingGrid),getEmpty(startingGrid))   
 queue =[]
-heapq.heappush(queue,(0,gridPath([startingGrid,'S']))) 
+heapq.heappush(queue,(0,(startingGrid,''))) 
 print(queue)
-i=0
+path = 'S'
+steps=0
 while(True):
-    i+=1
-    if(i%10000==0): print(i)
-    weight,temp = heapq.heappop(queue)
-    grid,path =temp.data()
+    steps+=1
+    weight,(grid,path) = heapq.heappop(queue)
+    if(steps%10000==0):
+        print("\nSteps: "+str(steps))
+        print("Queue Size: "+str(len(queue)))
+        print("Depth: "+str(len(path)))
+        print("Weight: "+str(weight))
+        print("Path: "+str(path))
+        print("Grid: " +str(grid))
     if(grid==targetGrid):
-        print(weight)
         print(path)
         print(grid)
+        print(steps)
         break
     #print(path)
+    '''
+    direction = input("\n")
+    match direction:
+        case 'u':
+            startingGrid,path = moveUp(startingGrid,path)   
+        case 'd':
+            startingGrid,path = moveDown(startingGrid,path)
+        case 'l':
+            startingGrid,path = moveLeft(startingGrid,path)
+        case 'r':
+            startingGrid,path = moveRight(startingGrid,path)
+    '''
     branches = branch(grid,path)
     for child in branches:
-        heapq.heappush(queue,(weight+1,gridPath(child)))
+        unique = True
+        for i in queue:
+            if(i[1][0]==child[0]):
+                unique = False
+                break
+        if(unique):
+            heapq.heappush(queue,(weight+1,child))
     
 
 read_file("input.txt")

1bca3c0f682336451356f5b29eaf13b2770d177d
adithgopal24 <azg5941@psu.edu>
2023-09-19 21:02:20 -0400

    Update UCS.py

--- UCS.py
+++ UCS.py
@@ -1,1 +1,25 @@
 #UCS
+import numpy
+import pygame
+import heapq
+
+def read_file(file):
+    f = open(file, "r")
+    print(f.read())
+
+def ucs(initial_state):
+    queue = [(0, node(initial_state))]
+    class PriorityQueue(obj):
+        def __init__(self):
+            self.queue[]
+        def __str__(self):
+            for i in self.queue:
+                return ' '.join([str(i)])
+        def insert(self, state):
+            self.queue.append(state)
+        def delete(self):
+            try:
+                max_val = 0
+                for i in range(len(self.queue)):
+                    if self.queue[i] > self.queue[]
+read_file("input.txt")
